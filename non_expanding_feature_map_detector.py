e42ac6605e35b639c1d8a413c0809a9c020b6399
tajkiatoma <tajkiatoma@gmail.com>
2022-03-28 01:10:38 -0600

    Save detection result

--- non_expanding_feature_map_detector.py
+++ non_expanding_feature_map_detector.py
@@ -1,10 +1,11 @@
 import ast
-import os
+import csv
 from os import listdir
-from os.path import isfile, join
+from os.path import join
 from pprint import pprint
 
 from find_definition import FindDefinition
+from smelled_repo import SmelledRepo, SmellType
 
 
 def get_ast(file_path):
@@ -22,34 +23,52 @@ def get_conv2d_calls_in_groups(code_ast):
 
 
 def detect_non_expanding_feature_map(file_path):
-    detection_lineno = []
     code_ast = get_ast(file_path)
     call_groups = get_conv2d_calls_in_groups(code_ast)
 
+    file_name = path.split('\\')[1]
+    file_name_parts = file_name.split('$')
+
+    repo_name = "/".join([file_name_parts[0], file_name_parts[1]])
+    file_name = "\\".join(file_name_parts[2:])
+
+    smelled_lines = list()
     for calls in call_groups:
         for i in range(len(calls) - 1):
             if calls[i].filters_value > calls[i + 1].filters_value:
-                detection_lineno.append(calls[i + 1].line_no)
-    return detection_lineno
+                smelled_lines.append(SmelledRepo(repo_name, file_name, calls[i + 1].line_no, SmellType.NON_EXPANDING_FEATURE_MAP))
+    return smelled_lines
 
 
 if __name__ == '__main__':
     directory = "data/repo_files"
     files = listdir(directory)
+
     error_count = 0
     detection_count = 0
+
+    repo_list = list()
     for i in range(len(files)):
         file = files[i]
         path = join(directory, file)
-        print(f"[{i}] Processing file {path}")
+        print(f"[{i+1}] Processing file {path}")
         try:
-            detection_lineno = detect_non_expanding_feature_map(path)
-            if len(detection_lineno) > 0:
+            detected_lines = detect_non_expanding_feature_map(path)
+            if len(detected_lines) > 0:
                 detection_count += 1
-                print(f"Non-expanding feature map found in line no: {detection_lineno}")
+                for line in detected_lines:
+                    repo_list.append(line)
         except Exception as e:
             error_count += 1
             print(f"Error: {e}")
             continue
+
     print(f"Couldn't process {error_count} files")
-    print(f"Total {detection_count} files have non-expanding feature map smell")
+    print(f"Found {detection_count} files having non-expanding feature map")
+    pprint(repo_list)
+
+    with open("data/repositories_with_smells/non_expanding_feature_maps/repo_list.csv", 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Full Name", "File Name", "Line No."])
+        for repo in repo_list:
+            writer.writerow(repo.get_row())
---
+++ smelled_repo.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+
+class SmellType(Enum):
+    NON_REPRESENTATIVE_STATISTICS_ESTIMATION = 1
+    NON_EXPANDING_FEATURE_MAP = 2
+
+
+class SmelledRepo:
+    def __init__(self, full_name: str, file_name: str, line_no: int, smell_type: SmellType):
+        self.repo_full_name = full_name
+        self.smelled_file_name = file_name
+        self.smell_line_no = line_no
+        self.smell_type = smell_type
+
+    def __repr__(self):
+        return f"{self.repo_full_name}, {self.smelled_file_name}, {self.smell_line_no}"
+
+    def get_row(self):
+        return [self.repo_full_name, self.smelled_file_name, self.smell_line_no]
