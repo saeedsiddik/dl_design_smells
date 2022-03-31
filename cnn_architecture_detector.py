4fe56ddb0cc080274443fa4eb8900e6650c70ff7
tajkiatoma <tajkiatoma@gmail.com>
2022-03-31 11:38:53 -0600

    Add alexnet detection

--- cnn_architecture_detector.py
+++ cnn_architecture_detector.py
@@ -16,4 +16,18 @@ def is_unet(no_of_filters: list[Conv2DCall]):
         if no_of_filters[i].filters_value != no_of_filters[-i-2].filters_value:
             return False
 
+    print(f"--------------Detected U-Net from line {no_of_filters[0].line_no} to {no_of_filters[-1].line_no}")
     return True
+
+
+def is_alexnet(no_of_filters: list[Conv2DCall]):
+    if len(no_of_filters) == 5 and (no_of_filters[1].filters_value == no_of_filters[4].filters_value):
+        print(f"--------------Detected AlexNet from line {no_of_filters[0].line_no} to {no_of_filters[-1].line_no}")
+        return True
+    return False
+
+
+def is_specific_architecture(no_of_filters: list[Conv2DCall]):
+    if is_unet(no_of_filters) or is_alexnet(no_of_filters):
+        return True
+    return False
